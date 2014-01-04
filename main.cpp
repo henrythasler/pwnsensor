3d62a5c50291b37c8f64d03679ac3520e1a46bf7
henrythasler <git@thasler.org>
2014-01-04 12:25:09 +0100

    added window title

--- main.cpp
+++ main.cpp
@@ -8,11 +8,13 @@ int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
+
     qmlRegisterType<QLmSensors>("sensors", 1, 0, "QLmSensors");
     qmlRegisterType<QSensorItem>("sensors", 1, 0, "QSensorItem");
     qmlRegisterType<QSensorSample>("sensors", 1, 0, "QSensorSample");
 
     QtQuick2ApplicationViewer viewer;
+    viewer.setTitle("pwnsensor alpha-1");
     viewer.setMainQmlFile(QStringLiteral("qml/pwnsensor/main.qml"));
     viewer.showExpanded();
 
